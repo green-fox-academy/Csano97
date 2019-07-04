5a9545b84f2b9f192943db68db42096806b35b42
Csano97 <csano.kornel@gmail.com>
2019-07-04 09:06:50 +0200

    Add fix for Sum exercise

--- week-01/day-5/Sum.cpp
+++ week-01/day-5/Sum.cpp
@@ -3,16 +3,18 @@
 int sum(int a);
 
 int main(){
-    int number = 10;
-    //std::cout << "Give a number:";
-    //std::cin >> number;
+    int number;
+    std::cout << "Give a number:";
+    std::cin >> number;
     std::cout << sum(number) << std::endl;
 
     return 0;
 }
-int sum(int a){
-    int i;
-    for (i = 0; a > 0; i += a % 10, a /= 10);
-
-    return i;
+int sum(int a)
+{
+    int summar = 0;
+    for (int i = 1; i < a+1; ++i){
+        summar += i;
+    }
+    return summar;
 }
