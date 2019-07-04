f40d4308f4e77626d4e42730aade6e69739fc7a8
Csano97 <csano.kornel@gmail.com>
2019-07-04 09:27:26 +0200

    Add fix for Sum All

--- week-01/day-5/SumAll.cpp
+++ week-01/day-5/SumAll.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-int main(){
+int main()
+{
     int ai[] = {3, 4, 5, 6, 7};
+    int result = 0;
     int n = sizeof(ai) / sizeof(ai[0]);
     for (int i = 0; i < n; ++i) {
-        std::cout << ai[i] + ai[i] << std::endl;
-
+        result += ai[i];
     }
-
-
+    std::cout << result << std::endl;
     return 0;
 }
