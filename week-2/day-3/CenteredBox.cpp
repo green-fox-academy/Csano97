b885c5f306d1abd3e1c726d0d5410b5ccfe8728e
Csano97 <csano.kornel@gmail.com>
2019-07-04 10:43:34 +0200

    Add fix for Centered box exercise

--- week-2/day-3/CenteredBox.cpp
+++ week-2/day-3/CenteredBox.cpp
@@ -1,5 +1,7 @@
 #include "draw.h"
 
+const int SCREEN_WIDTH = 720;
+const int SCREEN_HEIGHT = 720;
 // create a function that draws one square and takes 1 parameters:
 // the square size
 // and draws a square of that size to the center of the canvas.
@@ -20,7 +22,9 @@ void draw(SDL_Renderer *gRenderer)
 
 void rectangle(SDL_Renderer *gRenderer, int s)
 {
+    int x = SCREEN_WIDTH / 2 - s / 2;
+    int y = SCREEN_HEIGHT / 2 -s / 2;
     SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
-    SDL_Rect SDL_RectEmpty = {270, 310, s, s};
+    SDL_Rect SDL_RectEmpty = {x, y, s, s};
     SDL_RenderDrawRect(gRenderer, &SDL_RectEmpty);
 }
