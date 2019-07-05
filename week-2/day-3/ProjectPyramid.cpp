eb67903f3aec744776008066498ac15afa5ba5e5
Csano97 <csano.kornel@gmail.com>
2019-07-05 14:59:28 +0200

    Add pyramid to project

---
+++ week-2/day-3/ProjectPyramid.cpp
@@ -0,0 +1,21 @@
+#include "draw.h"
+const int SCREEN_WIDTH = 650;
+const int SCREEN_HEIGHT = 650;
+void draw(SDL_Renderer *gRenderer)
+{
+    SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, 0xFF);
+    //SDL_RenderDrawLine(gRenderer, 0, 0, 200, 200);
+    //SDL_RenderDrawLine(gRenderer, 200, 0, 0, 200);
+    int size = 24;
+    int m = 24;
+    for (int i = 0; i < 100; ++i) {
+        int p1[2] = {SCREEN_WIDTH/2-(size/2)*i, i*m};
+        int p2[2] = {SCREEN_WIDTH/2+(size/2)*i, i*m};
+        int p3[2] ={size*i, SCREEN_HEIGHT};
+        int p4[2] ={SCREEN_WIDTH-size*i, SCREEN_HEIGHT};
+        SDL_RenderDrawLine(gRenderer, p1[0], p1[1], p2[0], p2[1]);
+        SDL_RenderDrawLine(gRenderer, p2[0], p2[1], p3[0], p3[1]);
+        SDL_RenderDrawLine(gRenderer, p1[0], p1[1], p4[0], p4[1]);
+        SDL_RenderDrawLine(gRenderer, p3[0], p3[1], p4[0], p4[1]);
+    }
+}
