42e7ed1dfca1f50c007d87c90686f59719f32116
Csano97 <csano.kornel@gmail.com>
2019-07-11 10:53:50 +0200

    Add solution to Pokemon

---
+++ week-3/day-3/PokemonMain.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+#include "pokemon.h"
+
+
+std::vector<Pokemon> initializePokemons()
+{
+    std::vector<Pokemon> pokemons;
+
+    pokemons.emplace_back("Balbasaur", "leaf", "water");
+    pokemons.emplace_back("Pikatchu", "electric", "water");
+    pokemons.emplace_back("Charizard", "fire", "leaf");
+    pokemons.emplace_back("Squirtle", "water", "fire");
+    pokemons.emplace_back("Kingler", "water", "fire");
+
+    return pokemons;
+}
+
+int main(int argc, char* args[])
+{
+    std::vector<Pokemon> pokemonOfAsh = initializePokemons();
+
+    // Every pokemon has a name and a type.
+    // Certain types are effective against others, e.g. water is effective against fire.
+
+    // Ash has a few pokemon.
+    // A wild pokemon appeared!
+
+    Pokemon wildPokemon("Oddish", "leaf", "water");
+
+    // Which pokemon should Ash use?
+    Pokemon chosen("Charizard", "fire", "leaf");
+
+
+    std::cout << "I choose You, " << chosen.getName() << "\n" << "Ash won: " << std::boolalpha << chosen.isEffectiveAgainst(wildPokemon) << std::endl;
+    return 0;
+}
