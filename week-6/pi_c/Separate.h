230a565d8d8ec846bea7f81070f388e64ae83ce1
Csano97 <csano.kornel@gmail.com>
2019-08-05 14:37:16 +0200

    add solution to separation

---
+++ week-6/pi_c/Separate.h
@@ -0,0 +1,5 @@
+#ifndef PI_C_SEPARATE_H
+#define PI_C_SEPARATE_H
+#define PI 3.14
+double circumference(double radius);
+#endif //PI_C_SEPARATE_H
--- week-6/pi_c/main.c
+++ week-6/pi_c/main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include "Separate.h"
 int main()
 {
-    double pi = 3.14;
+
     double radius = 0;
     double area;
     printf("Give the radius of the circle:\n");
     scanf("%lf", &radius);
-    area = radius * radius * pi;
+    area = radius * radius * PI;
     printf("Area of the circle: %lf\n", area);
+    printf("%lf", circumference(radius));
     return 0;
+}
+double circumference(double radius){
+    double circumference = 2 * radius * PI;
+    return circumference;
 }
