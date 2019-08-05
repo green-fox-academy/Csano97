92dd0e4bac6823fc6a8c0b8d9aeb9a0af97210b9
Csano97 <csano.kornel@gmail.com>
2019-08-05 13:42:59 +0200

    Add solution to two numbers

---
+++ week-6/two numbers/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int two_numbers_results;
+    two_numbers_results = 22 + 13;
+    printf("%d\n", two_numbers_results);
+    two_numbers_results = 22 - 13;
+    printf("%d\n", two_numbers_results);
+    two_numbers_results = 22 * 13;
+    printf("%d\n", two_numbers_results);
+    float two_numbers_float =  22.0 / 13.0;
+    printf("%.2f\n", two_numbers_float);
+    return 0;
+}
