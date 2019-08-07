57fe9820b1dcb7e14d77864b545f3444307e0744
Csano97 <csano.kornel@gmail.com>
2019-08-07 13:30:37 +0200

    Add solution to calloc

--- week-6/day-3/calloc/main.c
+++ week-6/day-3/calloc/main.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// with malloc
+// with calloc
 // please allocate a 10 long array and fill it with 5!! numbers from 0 to 4, then print the whole array
 // please delete the array before the program exits
+// what's the difference between this and the previous exercise?
 
 int main()
 {
-    int* pointer = NULL;
-    pointer = (int*)malloc(5 * sizeof(int));
+    int *pointer = NULL;
+    pointer = (int *) calloc(5, sizeof(int));
     for (int i = 0; i < 5; ++i) {
         pointer[i] = i;
         printf("%d ", pointer[i]);
     }
+    printf("\n");
     free(pointer);
+    for (int j = 0; j < 5; ++j) {
+        printf("%d ", pointer[j]);
+    }
     return 0;
 }
